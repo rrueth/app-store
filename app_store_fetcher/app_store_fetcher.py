1e1d50e7a139628207757169b9488f2403b75c88
Ryan Rueth <rrueth@gmail.com>
2016-03-06 22:04:47 -0800

    Initial code commit
    Contains a basic script that fetches a few pages from the app store and
    is able to parse the reviews.

---
+++ app_store_fetcher/app_store_fetcher.py
@@ -0,0 +1,88 @@
+import requests
+
+
+class Review(object):
+    """A JSON review is made up of:
+    - author:
+        - uri
+            - label (What exactly does this point to?)
+        - name
+            - label (user name writing the review)
+        - label (Seems to be empty)
+    - im:version:
+        - label (version string)
+    - im:rating:
+        - label (1 - 5 representing the number of stars given by the reviewer)
+    - id:
+        - label (string representing the ID of the review?)
+    - title:
+        - label (review title)
+    - content:
+        - label (the primary review)
+        - attributes:
+            - type ("text", when would this be anything other than "text?")
+    - link
+        - attributes
+            - rel ("related")
+            - href (links to something? related apps in this case?)
+    - im:voteSum:
+        - label ("0", not sure what the vote sum is yet)
+    - im:contentType:
+        - attributes:
+            - term ("Application")
+            - label ("Application")
+    - im:voteCount:
+        - label ("0", not sure what this is yet)
+    """
+    def __init__(self, json_dict):
+        self.json_dict = json_dict
+
+    @staticmethod
+    def is_review(json_dict):
+        """
+        :param json_dict: A JSON object represented as a dictionary.
+        :return: True if the json_dict represents a review.
+        """
+        return bool("im:rating" in json_dict)
+
+    def author_name(self):
+        return self.json_dict["author"]["name"]["label"]
+
+    def content(self):
+        return self.json_dict["content"]["label"]
+
+    def rating(self):
+        """
+        :return: An integer from 1 to 5 denoting the number of stars given by the reviewer.
+        """
+        return int(self.json_dict["im:rating"]["label"])
+
+    def title(self):
+        return self.json_dict["title"]["label"]
+
+    def version(self):
+        return self.json_dict["im:version"]["label"]
+
+
+if __name__ == "__main__":
+    # TODO - Ryan - Eventually break this down and pass the url parameters properly
+    # TODO - Ryan - Implement following next/previous links.
+    content_strings = []
+    urls = ["https://itunes.apple.com/us/rss/customerreviews/id=383298204/sortBy=mostRecent/json"]
+    for i in range(1, 11):
+        urls.append(
+            "https://itunes.apple.com/us/rss/customerreviews/page={page_num}/id=383298204/sortby=mostrecent/json".format(
+                page_num=i,
+            ))
+    # urls =[]
+    for url in urls:
+        print(url)
+        r = requests.get(url)
+
+        # TODO - Ryan:
+        # - Parse the JSON
+        feed_json = r.json()["feed"]
+        # print(feed_json)
+        for entry in feed_json["entry"]:
+            if Review.is_review(entry):
+                content_strings.append(Review(entry).content())
---
+++ app_store_fetcher/app_store_fetcher_test.py
@@ -0,0 +1,55 @@
+import json
+import unittest
+
+import app_store_fetcher
+
+REVIEW_ENTRY = """{
+  "im:version": {"label": "4.8.1"},
+  "title": {"label": "Shop kick"},
+  "author": {"label": "",
+             "uri": {"label": "https://itunes.apple.com/us/reviews/id171860631"},
+             "name": {"label": "Awesome he"}},
+  "im:voteCount": {"label": "0"},
+  "content": {"attributes": {"type": "text"},
+              "label": "I love this but it does not have a lot of stuff that in our area on it."},
+  "link": {"attributes": {"href": "https://itunes.apple.com/us/review?id=383298204&type=Purple%20Software",
+                          "rel": "related"}},
+  "im:rating": {"label": "5"},\
+  "im:contentType": {"attributes": {"term": "Application",
+                                    "label": "Application"}},
+  "id": {"label": "1330930867"},
+  "im:voteSum": {"label": "0"}
+}
+"""
+
+class TestReview(unittest.TestCase):
+    def _review(self, json_dict=None):
+        json_dict = json_dict or json.loads(REVIEW_ENTRY)
+        return app_store_fetcher.Review(json_dict)
+
+    # TODO - Ryan - add test for is_review function
+
+    def test_should_return_author_name(self):
+        review = self._review()
+        self.assertEqual("Awesome he", review.author_name())
+
+    def test_should_return_content(self):
+        review = self._review()
+        self.assertEqual("I love this but it does not have a lot of stuff that in our area on it.",
+                         review.content())
+
+    def test_should_return_rating(self):
+        review = self._review()
+        self.assertEqual(5, review.rating())
+
+    def test_should_return_title(self):
+        review = self._review()
+        self.assertEqual("Shop kick", review.title())
+
+    def test_should_return_version(self):
+        review = self._review()
+        self.assertEqual("4.8.1", review.version())
+
+
+if __name__ == "__main__":
+    unittest.main()
