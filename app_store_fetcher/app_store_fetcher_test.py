5983dda6ca5ad3d5fda79a7a21d99b3987f9d9cc
Ryan Rueth <rrueth@gmail.com>
2016-03-13 18:08:42 -0700

    Move the _review function outside of TestReview.
    Move the _review function outside of TestReview.

--- app_store_fetcher/app_store_fetcher_test.py
+++ app_store_fetcher/app_store_fetcher_test.py
@@ -22,32 +22,33 @@ REVIEW_ENTRY = """{
 }
 """
 
+
+def _review(json_dict=None):
+    json_dict = json_dict or json.loads(REVIEW_ENTRY)
+    return app_store_fetcher.Review(json_dict)
+
+
 class TestReview(unittest.TestCase):
-    def _review(self, json_dict=None):
-        json_dict = json_dict or json.loads(REVIEW_ENTRY)
-        return app_store_fetcher.Review(json_dict)
-
     # TODO - Ryan - add test for is_review function
-
     def test_should_return_author_name(self):
-        review = self._review()
+        review = _review()
         self.assertEqual("Awesome he", review.author_name())
 
     def test_should_return_content(self):
-        review = self._review()
+        review = _review()
         self.assertEqual("I love this but it does not have a lot of stuff that in our area on it.",
                          review.content())
 
     def test_should_return_rating(self):
-        review = self._review()
+        review = _review()
         self.assertEqual(5, review.rating())
 
     def test_should_return_title(self):
-        review = self._review()
+        review = _review()
         self.assertEqual("Shop kick", review.title())
 
     def test_should_return_version(self):
-        review = self._review()
+        review = _review()
         self.assertEqual("4.8.1", review.version())
 
 
